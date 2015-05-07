e0b2dc6fea40cf885783db7a68e85a554135b049
JT Gryder <jgryder@calpoly.edu>
2015-05-07 16:40:09 -0700

    changed battle background for now

--- ForestBattle.java
+++ ForestBattle.java
@@ -24,7 +24,7 @@ public class ForestBattle extends BattleField
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super();
         showText("Monster appeared!", 400, 360);
-        setBackground("images/burlap.jpg");
+        setBackground("images/map/tree_leaves.png");
 
         createEnemies(enemies);
 
