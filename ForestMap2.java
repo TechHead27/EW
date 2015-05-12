416526a5968bd4786239fa7078b41bfd6349ecdd
Darren Huang <darren12_11@yahoo.com.tw>
2015-05-12 15:55:21 -0700

    Added ForestMap2

--- ForestMap2.java
+++ ForestMap2.java
@@ -1,58 +1,51 @@
 import greenfoot.*;
 
 /**
- * Write a description of class ForestMap1 here.
+ * Write a description of class ForestMap2 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ForestMap1 extends World
+public class ForestMap2 extends World
 {
-
     private Character player;
     private java.util.Random gen;
     private GreenfootSound bgm;
     /**
-     * Constructor for objects of class ForestMap1.
+     * Constructor for objects of class ForestMap2.
      * 
      */
-    public ForestMap1()
-    {
+    public ForestMap2()
+    {    
         // Create a new world with 12x8 cells with a cell size of 50x50 pixels.
-        super(12, 8, 50);
-        player = new Character(11, 7);
-        addObject(player, 11, 7);
+        super(12, 8, 50); 
+        player = new Character(6, 7);
+        addObject(player, 0, 0);
         Greenfoot.delay(15);
         gen = new java.util.Random();
-        bgm = new GreenfootSound("BGM/Forest1.mp3");
+        bgm = new GreenfootSound("BGM/Mansion1.mp3");
         bgm.playLoop();
-        setBackground("images/map/tombstone.png");
+        setBackground("images/map/tree.png");
     }
     
-    public ForestMap1(Character c)
+    public ForestMap2(Character c)
     {
         // Create a new world with 12x8 cells with a cell size of 50x50 pixels.
         super(12, 8, 50);
         player = c;
         player.setMovable(true);
         player.exploreReady();
-        addObject(player, 300, 200);
+        addObject(player, 0,0);
         //Greenfoot.delay(15); Seems unnecessary? Not exactly sure what it was for
         gen = new java.util.Random();
-        bgm = new GreenfootSound("BGM/Forest1.mp3");
+        bgm = new GreenfootSound("BGM/Mansion1.mp3");
         bgm.playLoop();
-        setBackground("images/map/tombstone.png");
+        setBackground("images/map/tree.png");
     }
     
     public void act()
     {
-        if (player.hasMoved() && player.getY() == 0)
-        {
-            Greenfoot.delay(7);
-            Greenfoot.setWorld(new ForestMap2());
-            bgm.stop();
-        }
-        else if (player.hasMoved() && gen.nextFloat() <= 0.13)
+        if (player.hasMoved() && gen.nextFloat() <= 0.20)
         {
             Greenfoot.delay(7);
             Greenfoot.setWorld(new ForestBattle(player, Greenfoot.getRandomNumber(4)));
