931136ee744175c4b3adb07dec5f70545a704b1c
JT Gryder <jgryder@calpoly.edu>
2015-05-14 16:41:11 -0700

    go beat the final boss!

--- FinalBattle.java
+++ FinalBattle.java
@@ -37,7 +37,7 @@ public class FinalBattle extends BattleField
         faithBar = new FaithBar(player);
         addObject(faithBar, 225, 20);
         
-        bgm = new GreenfootSound("BGM/BattleTheme1.mp3");
+        bgm = new GreenfootSound("BGM/Boss.mp3");
         bgm.playLoop();
         Greenfoot.delay(35);
         player = c;
@@ -48,26 +48,26 @@ public class FinalBattle extends BattleField
         showText("", 400, 360);
         if (Greenfoot.isKeyDown("7"))
         {
-            /**bgm.stop();
+            bgm.stop();
             removeObject(boss);
             GreenfootSound victory = new GreenfootSound("BGM/VictoryFanFare.mp3");
             victory.playLoop();
-            showText("YOU HAVE PROTECTED THE WORLD FROM",300,273);
+            
+            showText("YOU HAVE PROTECTED THE WORLD FROM",300,270);
             showText("THE EVIL POSSESED TOILET.",300,290);
-            //while(!Greenfoot.isKeyDown("enter")) {
-                // just don't do anything!
-                showText("YOU HAVE PROTECTED THE WORLD FROM",300,273);
-                showText("THE EVIL POSSESED TOILET.",300,290);
-            //}
-            showText("YOU HAVE FREED THE PEOPLE OF THEIR FEAR", 300, 273);
-            showText("OF POOPING AND CLOSED THE SWIRLING VORTEX WITHIN.", 300, 290);
-            //while(!Greenfoot.isKeyDown("enter")) {
-                // just don't do anything again!
-                showText("YOU HAVE FREED THE PEOPLE OF THEIR FEAR", 300, 273);
-                showText("OF POOPING AND CLOSED THE SWIRLING VORTEX WITHIN.", 300, 290);
-            //}
+            Greenfoot.delay(100);
+            
+            while(!Greenfoot.isKeyDown("enter")) {
+                showText("YOU HAVE FREED THE PEOPLE OF THEIR FEAR OF", 300, 270);
+                showText("POOPING AND CLOSED THE SWIRLING VORTEX WITHIN.", 300, 290);
+            }
+            Greenfoot.delay(50);
+            while(!Greenfoot.isKeyDown("enter")) {
+                
+            }
+            Greenfoot.delay(5);
             victory.stop();
-            Greenfoot.setWorld(new Credits("images/credits/credits1.png"));*/
+            Greenfoot.setWorld(new Credits("images/credits/credits1.png"));
         }
     }
 }
